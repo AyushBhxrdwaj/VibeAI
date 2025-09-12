318ed9af7b4590921c963cd906233148b26bab44
Ayush Bhardwaj <ayush.ds321@gmail.com>
2025-09-12 23:41:14 +0530

    Nav and some bug fixes

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -14,7 +14,7 @@ const HomeView = () => {
   }
   return (
 
-    <div className='flex flex-col gap-y-4'>
+    <div className='flex flex-col gap-y-4 p-4'>
       <p>Logged in as {session.user.name}</p>
       <Button onClick={()=>authClient.signOut({
         fetchOptions:{
