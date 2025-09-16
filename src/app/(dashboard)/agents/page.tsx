a2bfff7f418befade25a283e52bcb5ca7f05e3eb
Ayush Bhardwaj <ayush.ds321@gmail.com>
2025-09-16 22:56:17 +0530

    agent form

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -1,36 +1,51 @@
 import ErrorState from "@/components/error-state";
 import LoadingState from "@/components/loading-state";
+import { auth } from "@/lib/auth";
+import ListHeader from "@/modules/agents/ui/components/list-header";
 import AgentsView from "@/modules/agents/ui/views/agents-view";
 import { getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 import React, { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 
 const page = async () => {
+  const session=await auth.api.getSession({
+    headers:await headers()
+  });
+  if(!session){
+    redirect('/sign-in')
+  }
+
   const queryClient = getQueryClient();
   void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense
-        fallback={
-          <LoadingState
-            title="Loading Agents"
-            description="This may take a few seconds...."
-          />
-        }
-      >
-        <ErrorBoundary
+    <>
+    <ListHeader/>
+    
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense
           fallback={
-            <ErrorState
-              title="Something went wrong"
-              description="Please try again later or contact support if the issue persists."
+            <LoadingState
+              title="Loading Agents"
+              description="This may take a few seconds...."
             />
           }
         >
-          <AgentsView />
-        </ErrorBoundary>
-      </Suspense>
-    </HydrationBoundary>
+          <ErrorBoundary
+            fallback={
+              <ErrorState
+                title="Something went wrong"
+                description="Please try again later or contact support if the issue persists."
+              />
+            }
+          >
+            <AgentsView />
+          </ErrorBoundary>
+        </Suspense>
+      </HydrationBoundary>
+    </>
   );
 };
 
