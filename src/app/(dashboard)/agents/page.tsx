26f9f86015e38cf1556bbe1d8808ac9663495426
Ayush Bhardwaj <ayush.ds321@gmail.com>
2025-09-19 23:13:20 +0530

    Pagination and search filter

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -1,16 +1,23 @@
 import ErrorState from "@/components/error-state";
 import LoadingState from "@/components/loading-state";
 import { auth } from "@/lib/auth";
+import { loadSearchParams } from "@/modules/agents/params";
 import ListHeader from "@/modules/agents/ui/components/list-header";
 import AgentsView from "@/modules/agents/ui/views/agents-view";
 import { getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
+import { SearchParams } from "nuqs";
 import React, { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 
-const page = async () => {
+interface Props{
+  searchParams:Promise<SearchParams>;
+}
+
+const page = async ({searchParams}:Props) => {
+  const filters=await loadSearchParams(searchParams);
   const session=await auth.api.getSession({
     headers:await headers()
   });
@@ -19,7 +26,9 @@ const page = async () => {
   }
 
   const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
+  void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions({
+    ...filters
+  }));
   return (
     <>
     <ListHeader/>
