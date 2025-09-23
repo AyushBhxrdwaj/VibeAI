20fcf24628874b13332d911af642e8a9d922e85b
Ayush Bhardwaj <ayush.ds321@gmail.com>
2025-09-23 23:28:55 +0530

    Meetings schema

---
+++ src/modules/meetings/ui/views/meetings-view.tsx
@@ -0,0 +1,34 @@
+'use client'
+import ErrorState from '@/components/error-state';
+import LoadingState from '@/components/loading-state';
+import { useTRPC } from '@/trpc/client'
+import { useSuspenseQuery } from '@tanstack/react-query';
+import React from 'react'
+
+const MeetingsView = () => {
+    const trpc=useTRPC();
+    const {data}=useSuspenseQuery(trpc.meetings.getMany.queryOptions({}))
+
+  return (
+    <div>{JSON.stringify(data)}</div>
+  )
+}
+
+export default MeetingsView
+
+export const MeetingsViewError = () => {
+  return (
+    <ErrorState
+      title="Something went wrong"
+      description="Please try again later or contact support if the issue persists."
+    />
+  );
+};
+export const MeetingsViewLoading = () => {
+  return (
+    <LoadingState
+      title="Loading Meetings"
+      description="This may take a few seconds...."
+    />
+  );
+};
